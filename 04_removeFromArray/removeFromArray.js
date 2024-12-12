26167fb9774238b4ee52fd639e7715aeb87a1608
Mihailo <183983866+mixa63@users.noreply.github.com>
2024-12-12 18:19:36 +0100

    Add solutions to the first 7 javascript exercises.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+
+    numbers.forEach(element => {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    });
+    
+    return array;
+
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  conversion = (temp - 32) * 5/9;
+  return parseFloat(conversion.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  conversion = temp * 9/5 + 32;
+  return parseFloat(conversion.toFixed(1));
 };
 
 // Do not edit below this line
